f00a66e7213b15c05d92dec37b39e4bc489343dc
chacasaguay <158518298+chacasaguay@users.noreply.github.com>
2024-02-01 17:13:33 -0500

    first commit

---
+++ R/hello.R
@@ -0,0 +1,15 @@
+
+
+hello <- function() {
+  print("Hello, world!")
+}
+
+# Función para determinar si un número es par o impar
+es_par_o_impar <- function(numero) {
+  if (numero %% 2 == 0) {
+    return("Par")
+  } else {
+    return("Impar")
+  }
+}
+
